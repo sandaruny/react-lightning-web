755d2ff4c61ced87b8ca9bc650d7dcbe651228ed
sandaruwann <Sandaruwan.Nanayakara@trycake.com>
2017-10-21 11:54:50 +0530

    feat(component): Add DropDownMenu component

--- src/App.js
+++ src/App.js
@@ -8,18 +8,25 @@ import Badge from './components/Badge';
 import TextArea from './components/TextArea';
 import TextFieldInput from './components/TextFieldInput';
 import TextField from './components/TextFieldInput';
-import Breadcrumb from './components/Breadcrumb';
+import DropDownMenu from './components/DropDownMenu';
 
-const breadcrumbs = [
-  { name: "PARENT ENTITY", link: "http://google.com" },
-  { name: "PARENT RECORD NAME", link: "http://google.com" },
+const items1 = [
+  { name: "Menu Item 1", onClick: () => { console.log('clicked')} },
+  { name: "Menu Item 2", onClick: () => { console.log('clicked')} },
+  { name: "Menu Item 3", onClick: () => { console.log('clicked')} },
 ];
+
+const items2 = [
+  { name: "Menu Item 1", onClick: () => { console.log('clicked')} },
+];
+
+const items = [ items1, items2 ];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Breadcrumb breadcrumbs={breadcrumbs}/>
+        <DropDownMenu menuName='Menu Name' items={items}/>
       </div>
     );
   }
